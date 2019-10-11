f78d6d9aa658686e7c176a8fc454873cded4eec7
zunkun <liuzunkun@gmail.com>
2019-10-11 16:59:46 +0800

    first commit

---
+++ config/development.js
@@ -0,0 +1,6 @@
+module.exports = {
+	PORT: 4532,
+	secret: 'whu',
+	videoPath: '/apps/files/videos/',
+	imagePath: '/apps/files/images/'
+};
---
+++ config/production.js
@@ -0,0 +1,6 @@
+module.exports = {
+	PORT: 4530,
+	secret: 'whu',
+	videoPath: '/apps/files/videos/',
+	imagePath: '/apps/files/images/'
+};
